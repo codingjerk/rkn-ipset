1bc55a0049b10cca012573d595630a9e08e711d6
Stanislav aka sbkubric Bogatskiy <sbkubric@gmail.com>
2022-12-27 00:02:35 +0300

    Updated script && added ipv6 support

--- nets2ipset.py
+++ nets2ipset.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 import fileinput
+from re import compile
+
+ipv4 = compile("^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$")
 
 for net in fileinput.input():
-    print("add blocked %s -exist" % net.strip())
+    stripped_net = net.strip()
+    if ipv4.search(stripped_net):
+        print(f"add blocked {stripped_net} -exist")
+    else:
+        print(f"add blocked6 {stripped_net} -exist")
+
